90dd30c4fed55d8dbd636822841c225675fbabed
Dattobias <152405844+datto2530@users.noreply.github.com>
2024-01-03 15:53:32 +0700

    da xong

---
+++ Chuyen doi nhiet do/ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice = -1;
+            while (choice != 0)
+            {
+                Console.WriteLine("Nhap lua chon");
+                choice = int.Parse(Console.ReadLine());
+                while (choice != 0)
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            {
+                               Console.WriteLine(CelsiusToFahrenheit());
+                            }
+                            break;
+                        case 2:
+                            {
+                                FahrenheitToCelsius();
+                            }
+                            break;
+                        case 3:
+                            {
+                                Environment.Exit(0);
+                            }
+                            break;
+                        default:
+                            {
+                                Console.WriteLine("Vui long nhap lai");
+                            }
+                            break;
+                    }
+                }
+            }
+        }
+         static double CelsiusToFahrenheit()
+        {
+            Console.WriteLine("Nhap do C");
+            double celsius = double.Parse(Console.ReadLine());
+            double fahrenheit = 9 / 5 * celsius + 32;
+            return fahrenheit;
+        }
+        static double FahrenheitToCelsius()
+        {
+            Console.WriteLine("Nhap do F");
+            double fahrenheit = double.Parse(Console.ReadLine());
+            double celsius = 5 / 9 * (fahrenheit - 32); ;
+            return celsius;
+        }
+    }
+}
